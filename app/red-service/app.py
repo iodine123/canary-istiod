f6031e3f269700e0d2a5c380b47fc715f332417d
iodine123 <iodinehanifan@gmail.com>
2025-10-25 10:33:52 +0700

    Feat!: change to microservices

--- app/red-service/app.py
+++ app/red-service/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Ini aplikasi v1"
+    return "Red service version 1.0"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
